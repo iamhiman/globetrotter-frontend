4091ab4efc839fc21eaa946fa26ddf9471b204d0
Himanshu Kashyap <himanshu27kashyap@gmail.com>
2025-03-01 18:12:00 +0530

    Add Game Background UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,21 @@
+import classNames from 'classnames/bind';
 import styles from './page.module.scss';
 
+const cx = classNames.bind(styles);
+
 export default function Home() {
-  return <main className={styles.main}>hello</main>;
+  return (
+    <main className={cx('main-container')}>
+      <section className={cx('homepage-section')}>
+        <div className={cx('box')}>
+          <div className={cx('square')} />
+          <div className={cx('square')} />
+          <div className={cx('square')} />
+          <div className={cx('container')}>
+            <h2>Globetrotter - The Ultimate Travel Guessing Game!</h2>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 }
