72b6961e43921f0a078c9661bc73c828296fbe17
Himanshu Kashyap <himanshu27kashyap@gmail.com>
2025-03-01 16:10:24 +0530

    Setup prettier and eslint

---
+++ src/app/metadata.ts
@@ -0,0 +1,6 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Globetrotter - Travel Guessing Game',
+  description: 'Guess famous destinations based on cryptic clues!',
+};
